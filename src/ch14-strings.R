f598483fbf73221818b6e7f5910ee30b85d53685
HeartlandNetwork <gareth_rowell@nps.gov>
2024-03-04 12:56:31 -0600

    initial strings

--- src/ch14-strings.R
+++ src/ch14-strings.R
@@ -5,3 +5,24 @@
 library(tidyverse)
 library(babynames)
 
+# 14.2 Creating a string -------------------------------------------------------
+
+string1 <- "This is a string"
+string2 <- 'If I want to include a "quote" inside a string, I use single quotes'
+
+
+# 14.2.1 Escapes ---------------------------------------------------------------
+
+double_quote <- "\"" # or '"'
+single_quote <- '\'' # or "'"
+
+backslash <- "\\"
+
+x <- c(single_quote, double_quote, backslash)
+x
+
+
+str_view(x)
+
+
+
