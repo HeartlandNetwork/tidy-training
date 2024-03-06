c36341e7e290b4659e17b189809170cb4195d370
HeartlandNetwork <gareth_rowell@nps.gov>
2024-03-06 11:57:09 -0600

    my new edit

--- src/ch14-strings.R
+++ src/ch14-strings.R
@@ -8,6 +8,7 @@ library(babynames)
 
 # 14.2 Creating a string -------------------------------------------------------
 
+
 string1 <- "This is a string"
 string2 <- 'If I want to include a "quote" inside a string, I use single quotes'
 
