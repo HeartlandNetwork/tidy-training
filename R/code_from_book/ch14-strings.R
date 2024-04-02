894bfb71c4fac9615286e770d9bc6b605adec84d
HeartlandNetwork <gareth_rowell@nps.gov>
2024-04-02 13:35:29 -0500

    encoded letters

---
+++ R/code_from_book/ch14-strings.R
@@ -0,0 +1,384 @@
+
+# Ch14 - Strings
+#===============================================================================
+
+library(tidyverse)
+library(babynames)
+
+
+# 14.2 Creating a string -------------------------------------------------------
+
+
+string1 <- "This is a string"
+string1
+string2 <- 'If I want to include a "quote" inside a string, I use single quotes'
+string2
+
+# 14.2.1 Escapes ---------------------------------------------------------------
+
+double_quote <- "\"" # or '"'
+double_quote 
+single_quote <- '\'' # or "'"
+single_quote
+
+backslash <- "\\"
+backslash
+
+x <- c(single_quote, double_quote, backslash)
+x
+
+str_view(x)
+
+# 14.2.2 Raw strings -----------------------------------------------------------
+
+
+tricky <- "double_quote <- \"\\\"\" # or '\''
+single_quote <- '\\'' # or \"'\""
+
+str_view(tricky)
+
+# using a raw string
+
+tricky <- r"(double_quote <- "\"" # or '"' 
+single_quote <- '\'' # or "'")"
+str_view(tricky)
+
+# 14.2.3 Other special characters ----------------------------------------------
+
+# \u or \U for unicode
+
+x <- c("one\ntwo", "one\ttwo", "\u00b5", "\U0001f604")
+x
+str_view(x)
+
+
+# 14.3 Creating many strings from data -----------------------------------------
+
+# 14.3.1 str_c() ---------------------------------------------------------------
+
+str_c("x", "y")
+
+str_c("x", "y", "z")
+
+str_c("Hello ", c("John", "Susan"))
+
+
+
+df <- tibble(name = c("Flora", "David", "Terra", NA))
+df
+df |> mutate(greeting = str_c("Hi ", name, "!"))
+
+
+# use coalesce() to replace missing values either inside or outside of str_c()
+
+df |> 
+  mutate(
+    greeting1 = str_c("Hi ", coalesce(name, "you"), "!"),
+    greeting2 = coalesce(str_c("Hi ", name, "!"), "Hi!")
+  )
+
+# 14.3.2 str_glue() ------------------------------------------------------------
+
+df <- tibble(name = c("Flora", "David", "Terra", NA))
+
+df |> mutate(greeting = str_glue("{{Hi {name}!}}"))
+
+
+# 14.3.3 str_flatten() ---------------------------------------------------------
+
+str_flatten(c("x", "y", "z"))
+
+str_flatten(c("x", "y", "z"), ", ")
+
+str_flatten(c("x", "y", "z"), ", ", last = ", and ")
+
+
+# str_flatten() with summarize()
+
+df <- tribble(
+  ~ name, ~ fruit,
+  "Carmen", "banana",
+  "Carmen", "apple",
+  "Marvin", "nectarine",
+  "Terence", "cantaloupe",
+  "Terence", "papaya",
+  "Terence", "mandarin"
+)
+
+df |>
+  group_by(name) |> 
+  summarize(fruits = str_flatten(fruit, ", "))
+
+
+# 14.4 Extracting data from strings --------------------------------------------
+
+# df |> separate_longer_delim(col, delim)
+# df |> separate_longer_position(col, width)
+# df |> separate_wider_delim(col, delim, names)
+# df |> separate_wider_position(col, widths)
+
+# 14.4.1 Separating into rows --------------------------------------------------
+
+df1 <- tibble(x = c("a,b,c", "d,e", "f"))
+df1 |> 
+  separate_longer_delim(x, delim = ",")
+
+
+df2 <- tibble(x = c("1211", "131", "21"))
+df2 |> 
+  separate_longer_position(x, width = 1)
+
+
+# 14.4.2 Separating into columns -----------------------------------------------
+
+df3 <- tibble(x = c("a10.1.2022", "b10.2.2011", "e15.1.2015"))
+df3 |> 
+  separate_wider_delim(
+    x,
+    delim = ".",
+    names = c("code", "edition", "year")
+  )
+
+
+# omitting a column using NA
+
+df3 |> 
+  separate_wider_delim(
+    x,
+    delim = ".",
+    names = c("code", NA, "year") # note location of NA
+  )
+
+# wider position uses a named vector with widths
+
+df4 <- tibble(x = c("202215TX", "202122LA", "202325CA")) 
+df4 |> 
+  separate_wider_position(
+    x,
+    widths = c(year = 4, age = 2, state = 2)
+  )
+
+# 14.4.3 Diagnosing widening problems ------------------------------------------
+
+
+# not enough pieces ------------------------------------------------------------
+
+
+df <- tibble(x = c("1-1-1", "1-1-2", "1-3", "1-3-2", "1"))
+
+# The code below throws an error because two of the values were too short
+
+#  df |> 
+#    separate_wider_delim(
+#      x,
+#      delim = "-",
+#       names = c("x", "y", "z"
+#      )
+#   )
+
+# the error message is below the code
+
+#  Error in `separate_wider_delim()`:
+#  ! Expected 3 pieces in each element of `x`.
+#  ! 2 values were too short.
+#  ℹ Use `too_few = "debug"` to diagnose the problem.
+#  ℹ Use `too_few = "align_start"/"align_end"` to silence this message.
+#  Run `rlang::last_trace()` to see where the error occurred.
+
+# Implementing the debug code
+
+debug <- df |> 
+  separate_wider_delim(
+    x,
+    delim = "-",
+    names = c("x", "y", "z"),
+    too_few = "debug"   # <<<<<<<<<<<<<<<<<<<<<
+  )
+debug
+
+# filter out 'not okay'
+
+debug |> filter(!x_ok)
+
+
+# fill in the missing pieces with NAs 
+
+df |> 
+  separate_wider_delim(
+    x,
+    delim = "-",
+    names = c("x", "y", "z"),
+    too_few = "align_start"
+  )
+
+
+
+# too many pieces --------------------------------------------------------------
+
+df <- tibble(x = c("1-1-1", "1-1-2", "1-3-5-6", "1-3-2", "1-3-5-7-9"))
+
+#df |> 
+#  separate_wider_delim(
+#    x,
+#    delim = "-",
+#    names = c("x", "y", "z")
+#  )
+
+# throws error
+
+#Error in `separate_wider_delim()`:
+#  ! Expected 3 pieces in each element of `x`.
+#! 2 values were too long.
+#ℹ Use `too_many = "debug"` to diagnose the problem.
+#ℹ Use `too_many = "drop"/"merge"` to silence this message.
+#Run `rlang::last_trace()` to see where the error occurred.
+
+debug <- df |> 
+  separate_wider_delim(
+    x,
+    delim = "-",
+    names = c("x", "y", "z"),
+    too_many = "debug"
+  )
+
+debug |> filter(!x_ok)
+
+# two options
+
+
+df |> 
+  separate_wider_delim(
+    x,
+    delim = "-",
+    names = c("x", "y", "z"),
+    too_many = "drop"    # <<<<<<<<<<<<<<<<<<<
+  )
+
+
+
+df |> 
+  separate_wider_delim(
+    x,
+    delim = "-",
+    names = c("x", "y", "z"),
+    too_many = "merge"   # <<<<<<<<<<<<<<<<<<<
+  )
+
+
+# 14.5 Letters -----------------------------------------------------------------
+
+
+str_length(c("a", "R for data science", NA))
+
+babynames |>
+  count(length = str_length(name), wt = n)
+
+babynames |> 
+  filter(str_length(name) == 15) |> 
+  count(name, wt = n, sort = TRUE)
+
+# 14.5.2 Subsetting
+
+x <- c("Apple", "Banana", "Pear")
+str_sub(x, 1, 3)
+
+# negative values count from the back of the string
+
+str_sub(x, -3, -1)
+
+# too short handles without error
+
+str_sub("a", 1, 5)
+
+
+df <- babynames |> 
+  mutate(
+    first = str_sub(name, 1, 1),
+    last = str_sub(name, -1, -1)
+  )
+
+glimpse(df)
+
+
+
+# 14.6 Non-English text --------------------------------------------------------
+
+# 14.6.1 Encoding
+
+
+# American Standard Code for Information Interchange (ASCII)
+
+charToRaw("Hadley") 
+
+
+# Unicode Transformation Format – 8-bit (UTF-8)
+
+# here is a challengin string
+
+x1 <- "text\nEl Ni\xf1o was particularly bad this year"
+
+read_csv(x1)$text # this fails
+
+x2 <- "text\n\x82\xb1\x82\xf1\x82\xc9\x82\xbf\x82\xcd"
+
+read_csv(x2)$text # this also fails
+
+# The next two work....
+
+read_csv(x1, locale = locale(encoding = "Latin1"))$text # Neither of these works
+
+
+read_csv(x2, locale = locale(encoding = "Shift-JIS"))$text
+#> [1] "こんにちは"
+#> 
+
+# 14.6.2 Letter variations  ----------------------------------------------------
+
+u <- c("\u00fc", "u\u0308")
+str_view(u)
+#> [1] │ ü
+#> [2] │ ü
+#> 
+str_length(u)
+#> [1] 1 2
+str_sub(u, 1, 1)
+#> [1] "ü" "u"
+#> 
+#> 
+u[[1]] == u[[2]]
+#> [1] FALSE
+
+str_equal(u[[1]], u[[2]])
+#> [1] TRUE
+
+
+
+# 14.6.3 Locale-dependent functions --------------------------------------------
+
+# locale listing
+
+stringi::stri_locale_list()
+
+# Turkish has two i’s: with and without a dot. Since they’re two distinct 
+#  letters, they’re capitalized differently:
+
+str_to_upper(c("i", "ı"))
+#> [1] "I" "I"
+str_to_upper(c("i", "ı"), locale = "tr")
+#> [1] "İ" "I"
+#> 
+
+# in Czech, “ch” is a compound letter that appears after h in the alphabet.
+
+str_sort(c("a", "c", "ch", "h", "z"))
+#> [1] "a"  "c"  "ch" "h"  "z"
+str_sort(c("a", "c", "ch", "h", "z"), locale = "cs")
+#> [1] "a"  "c"  "h"  "ch" "z"
+#> 
+
+
+
+
+
+
+
---
+++ R/exercise_answers/ex-ch14-strings.R
@@ -0,0 +1,232 @@
+
+# Ch14 - Strings
+#===============================================================================
+
+library(tidyverse)
+library(babynames)
+
+
+# 14.2.4 Exercises -------------------------------------------------------------
+
+# 1. Create strings that contain the following values:
+  
+#   1. He said "That's amazing!"
+
+my_string <- 'He said "That\'s amazing!"'
+my_string2 <- str_view(my_string)
+my_string2
+
+
+#   2. \a\b\c\d
+
+my_string <- "\\a\\b\\c\\d"
+my_string2 <- str_view(my_string)
+my_string2
+
+#   3. \\\\\\
+
+my_string <- "\\\\\\\\\\\\"
+my_string2 <- str_view(my_string)
+my_string2
+
+# 2. Create the string in your R session and print it. What happens to the 
+# special “\u00a0”? How does str_view() display it? Can you do a little 
+# googling to figure out what this special character is?
+  
+  x <- "This\u00a0is\u00a0tricky"
+  str_view(x)
+  x
+  
+  # 00a0 is unicode for "No Break Space"
+  # It prevents an automatic line break in word processing software
+  
+  
+  
+# 14.3.4 Exercises -------------------------------------------------------------
+  
+  
+# 1 Compare and contrast the results of paste0() with str_c() for the 
+# following inputs:
+  
+# ?paste0()
+
+str_c("hi ", NA)
+paste0("hi ", NA)
+  
+# 2. What’s the difference between paste() and paste0()? How can you recreate 
+# the equivalent of paste() with str_c()?
+
+paste0(1:12)
+paste(1:12, " ")  
+
+# paste() allows you to include characters to separate concatenated values. 
+# paste0() is the same as paste(..., "", collapse)
+
+
+    
+# 3. Convert the following expressions from str_c() to str_glue() or vice versa:
+
+food <- c("pepperoni pizza")
+price <- c("$23.00")
+#
+    
+str_c("The price of ", food, " is ", price)
+str_glue("The price of {food} is {price} ")
+
+age <- c(64)
+country <- c("the United States")
+  
+str_glue("I'm {age} years old and live in {country}")
+str_c("I'm ", age, " years old and live in ", country)
+
+title <- ("R for Data Science, 2nd edition")
+  
+str_c("\\section{", title, "}")
+str_glue("\\\\section{{{title}}}")
+
+
+# 14.5.3. Exercises -------------------------------------------------------------
+
+# 1. When computing the distribution of the length of babynames, why did we 
+# use wt = n?
+
+# wt = n was used with the count() function
+# https://dplyr.tidyverse.org/reference/count.html
+
+# from the reference above - wt to perform weighted counts, switching the 
+# summary from n = n() to n = sum(wt).
+
+
+# 2. Use str_length() and str_sub() to extract the middle letter from each 
+# baby name. What will you do if the string has an even number of characters?
+
+# the solution below uses integer division to locate the mid letter
+
+
+babynames |> 
+  mutate(
+    name_length = str_length(name),
+    ml = as.integer(name_length/2) + 1,
+    mid_letter = str_sub(name, ml, ml)
+  )
+
+
+
+# 3. Are there any major trends in the length of babynames over time? What 
+#    about the popularity of first and last letters? 
+
+
+df <- babynames |> 
+  mutate(
+    name_length = str_length(name),
+    first = str_sub(name, 1, 1),
+    last = str_sub(name, -1, -1)
+  )
+df
+
+# subsampling the dataset
+
+rand_df = df[sample(nrow(df), 10000), ]
+
+glimpse(rand_df)
+
+#----------- name_length ------------------------------------------------------
+# The length of baby names increases with time
+
+rand_df |>
+  group_by(year) |>
+  summarize(
+    max_name_length = max(name_length, na.rm = TRUE),
+    .groups = "drop"
+  ) |>
+  ggplot(aes(x = year, y = max_name_length)) +
+    geom_line()
+
+# ------------------------------------------------
+
+# first and last letter trends
+
+# assign years to four time periods
+
+period <- function(year){
+  case_when(
+    year < 1940 & year >= 1800 ~ "A",
+    year < 1980 & year >= 1940 ~ "B",
+    year < 2000 & year >= 1980 ~ "C",
+    year >= 2000 ~ "D"
+  )
+}
+
+rand_df <- rand_df |>
+  mutate(
+    period_group = period(year)
+  )
+rand_df
+
+# first letter trends ----------------------------------------------------------
+# calculating rel. frequencies of each letter by time period
+
+df <- rand_df |> 
+  group_by(first) |>
+  summarize(
+    n = n(),
+    propA = sum(period_group == 'A')/n,
+    propB = sum(period_group == 'B')/n,
+    propC = sum(period_group == 'C')/n,
+    propD = sum(period_group == 'D')/n
+  ) |>
+  pivot_longer(
+    cols = starts_with(("prop")),
+    names_to = "time_group",
+    values_to = "group_proportion"
+  )
+
+glimpse(df)
+
+# re. first letters of names
+# certain letters seem to been most common in the earlier years
+# including E, F O, V and W. Others that are more common in the 21st
+# century include A, B, L, x, Y, and Z
+
+library(ggthemes)
+
+# for last letter
+
+ggplot(df, aes(x = first, y = group_proportion, color = time_group)) +  
+  geom_point() +
+  scale_color_colorblind()
+
+# last letter trends ----------------------------------------------------------
+# calculating rel. frequencies of each letter by time period
+df <- rand_df |> 
+  group_by(last) |>
+  summarize(
+    n = n(),
+    propA = sum(period_group == 'A')/n,
+    propB = sum(period_group == 'B')/n,
+    propC = sum(period_group == 'C')/n,
+    propD = sum(period_group == 'D')/n
+  ) |>
+  pivot_longer(
+    cols = starts_with(("prop")),
+    names_to = "time_group",
+    values_to = "group_proportion"
+  )
+
+glimpse(df)
+
+# re. last letters of names
+# Only the letter p dominates in the early years. Q only shows up after 1979.
+# The letters b, h, j, v and x are really more common in recent years.
+
+library(ggthemes)
+
+
+ggplot(df, aes(x = last, y = group_proportion, color = time_group)) +  
+  geom_point() +
+  scale_color_colorblind()
+
+
+
+
+  
