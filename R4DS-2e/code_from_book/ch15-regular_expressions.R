6c74fc6afc019727bc89086708a5c20cb89ab4ed
HeartlandNetwork <gareth_rowell@nps.gov>
2024-05-20 15:43:27 -0500

    extract variables

---
+++ R4DS-2e/code_from_book/ch15-regular_expressions.R
@@ -0,0 +1,230 @@
+
+# Ch15 - Regular Expressions
+#===============================================================================
+
+
+# regex terminology -----
+
+  # prerequisites
+    # tidyverse
+    # babynames
+    # stringr char vectors
+      # fruit
+      # words
+      # sentences
+
+  # key functions
+    # str_detect()
+    # str_count()
+    # str_replace() and str_replace_all()
+    # extract variables
+      # separate_wider_regex()
+      # separate_wider_delim()
+      # separate_wider_position()
+
+  # pattern details
+    # escaping  
+    # anchors
+    # character classes or sets
+    # quantifiers
+    # operator precedence
+    # grouping and capturing
+      # capturing groups
+      # back reference
+
+  # pattern control
+    # regex flags
+      # flags
+    # fixed matches
+  
+  # practice
+    # check your work
+    # boolean operators
+    # creating a pattern with code
+    
+  # regex expressions in other places 
+    # tidyverse
+    # base R
+
+  # summary
+
+
+# prerequisites ----------------------------------------------------------------
+# tidyverse
+# babynames
+
+library(tidyverse)
+library(babynames)
+
+
+# stringr char vectors
+# fruit
+# words
+# sentences
+
+fruit
+
+words
+
+sentences
+
+# pattern basics ---------------------------------------------------------------
+
+# literal characters 
+
+str_view(fruit, "berry")
+
+
+# metacharacters 
+
+# ., +, *, [, ], and ?
+
+str_view(c("a", "ab", "ae", "bd", "ea", "eab"), "a.")
+
+str_view(fruit, "a...e")
+
+# quantifiers 
+
+# ab? matches an "a", optionally followed by a "b".
+str_view(c("a", "ab", "abb"), "ab?")
+
+# ab+ matches an "a", followed by at least one "b".
+str_view(c("a", "ab", "abb"), "ab+")
+
+
+# ab* matches an "a", followed by any number of "b"s.
+str_view(c("a", "ab", "abb"), "ab*")
+
+# character classes 
+# character classes are defined by []
+
+str_view(words, "[aeiou]x[aeiou]")
+
+# except [^abcd]
+
+str_view(words, "[^aeiou]y[^aeiou]")
+
+
+# alternation
+
+str_view(fruit, "apple|melon|nut")
+
+
+# stringr and tidyr functions -------------------------------------------------
+
+# str_detect() 
+
+str_detect(c("a", "b", "c"), "[aeiou]")
+
+babynames |> 
+  filter(str_detect(name, "x")) |> 
+  count(name, wt = n, sort = TRUE)
+
+babynames |> 
+  group_by(year) |> 
+  summarize(prop_x = mean(str_detect(name, "x"))) |> 
+  ggplot(aes(x = year, y = prop_x)) + 
+  geom_line()
+
+
+# str_count()
+
+x <- c("apple", "banana", "pear")
+str_count(x, "p")
+
+str_count("abababa", "aba")
+
+babynames |> 
+  count(name) |> 
+  mutate(
+    vowels = str_count(name, "[aeiou]"),
+    consonants = str_count(name, "[^aeiou]")
+  )
+
+babynames |> 
+  count(name) |> 
+  mutate(
+    name = str_to_lower(name),
+    vowels = str_count(name, "[aeiou]"),
+    consonants = str_count(name, "[^aeiou]")
+  )
+
+# str_replace() and str_replace_all()
+
+x <- c("apple", "pear", "banana")
+str_replace_all(x, "[aeiou]", "-")
+
+x <- c("apple", "pear", "banana")
+str_remove_all(x, "[aeiou]")
+
+# extract variables
+
+# separate_wider_regex()
+
+df <- tribble(
+  ~str,
+  "<Sheryl>-F_34",
+  "<Kisha>-F_45", 
+  "<Brandon>-N_33",
+  "<Sharon>-F_38", 
+  "<Penny>-F_58",
+  "<Justin>-M_41", 
+  "<Patricia>-F_84", 
+)
+
+df |> 
+  separate_wider_regex(
+    str,
+    patterns = c(
+      "<", 
+      name = "[A-Za-z]+", 
+      ">-", 
+      gender = ".",
+      "_",
+      age = "[0-9]+"
+    )
+  )
+
+# separate_wider_delim()
+# separate_wider_position()
+
+
+# pattern details --------------------------------------------------------------
+# escaping  
+# anchors
+# character classes or sets
+# quantifiers
+# operator precedence
+# grouping and capturing
+# capturing groups
+# back reference
+
+# pattern control
+# regex flags
+# flags
+# fixed matches
+
+# practice
+# check your work
+# boolean operators
+# creating a pattern with code
+
+# regex expressions in other places 
+# tidyverse
+# base R
+
+# summary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- R4DS-2e/code_from_book/ch15-regular_expressions_old.R
+++ R4DS-2e/code_from_book/ch15-regular_expressions_old.R
@@ -2,8 +2,9 @@
 # Ch15 - Regular Expressions
 #===============================================================================
 
+##################################
 
-# regex terminology -----
+# regex terminology
 
   # prerequisites
     # tidyverse
@@ -12,6 +13,14 @@
       # fruit
       # words
       # sentences
+
+  # pattern basics
+    # literal characters
+    # metacharacters
+    # quantifiers
+    # character classes
+      # except
+      # alternation
 
   # key functions
     # str_detect()
@@ -25,65 +34,34 @@
   # pattern details
     # escaping  
     # anchors
-    # character classes or sets
-    # quantifiers
-    # operator precedence
-    # grouping and capturing
-      # capturing groups
-      # back reference
-
-  # pattern control
-    # regex flags
-      # flags
-    # fixed matches
-  
-  # practice
-    # check your work
-    # boolean operators
-    # creating a pattern with code
     
-  # regex expressions in other places 
-    # tidyverse
-    # base R
-
-  # summary
 
 
-# prerequisites ----------------------------------------------------------------
-# tidyverse
-# babynames
+
+
+
 
 library(tidyverse)
 library(babynames)
 
+glimpse(fruit)
+glimpse(words)
+glimpse(sentences)
 
-# stringr char vectors
-# fruit
-# words
-# sentences
-
-fruit
-
-words
-
-sentences
-
-# pattern basics ---------------------------------------------------------------
-
-# literal characters 
+# 15.2 Pattern basics ----------------------------------------------------------
 
 str_view(fruit, "berry")
-
-
-# metacharacters 
-
-# ., +, *, [, ], and ?
 
 str_view(c("a", "ab", "ae", "bd", "ea", "eab"), "a.")
 
 str_view(fruit, "a...e")
 
-# quantifiers 
+# Quantifiers
+
+# ? makes a pattern optional (i.e. it matches 0 or 1 times)
+# + lets a pattern repeat (i.e. it matches at least once)
+# * lets a pattern be optional or repeat (i.e. it matches any number of times, 
+# including 0).
 
 # ab? matches an "a", optionally followed by a "b".
 str_view(c("a", "ab", "abb"), "ab?")
@@ -91,61 +69,352 @@ str_view(c("a", "ab", "abb"), "ab?")
 # ab+ matches an "a", followed by at least one "b".
 str_view(c("a", "ab", "abb"), "ab+")
 
-
 # ab* matches an "a", followed by any number of "b"s.
-str_view(c("a", "ab", "abb"), "ab*")
+str_view(c("a", "ab", "abb"), "ab*") # note this is broader than * 
+# in DOS or Access queries!!
 
-# character classes 
-# character classes are defined by []
+
+# character classes use [] -----------------
 
 str_view(words, "[aeiou]x[aeiou]")
 
-# except [^abcd]
-
+# invert the match by starting with ^
+# this means anything but...
+  
 str_view(words, "[^aeiou]y[^aeiou]")
 
-
-# alternation
+# alternation ---------------------------
 
 str_view(fruit, "apple|melon|nut")
 
+str_view(fruit, "aa|ee|ii|oo|uu")
 
-# stringr and tidyr functions -----
 
-# str_detect() 
+# 15.3 Key functions -----------------------------------------------------------
 
-# str_count()
-# str_replace() and str_replace_all()
-# extract variables
-# separate_wider_regex()
-# separate_wider_delim()
-# separate_wider_position()
+# 15.3.1 Detect matches
 
-# pattern details
-# escaping  
-# anchors
-# character classes or sets
-# quantifiers
-# operator precedence
-# grouping and capturing
-# capturing groups
-# back reference
+# str_detect() returns a logical vector 
 
-# pattern control
-# regex flags
-# flags
-# fixed matches
+str_detect(c("a", "b", "c"), "[aeiou]")
 
-# practice
-# check your work
-# boolean operators
-# creating a pattern with code
+#> [1]  TRUE FALSE FALSE
 
-# regex expressions in other places 
-# tidyverse
-# base R
 
-# summary
+
+babynames |> 
+  filter(str_detect(name, "x")) |> # <<<<< str_detect inside filter
+  count(name, wt = n, sort = TRUE)
+
+# sum(str_detect(x, pattern)) tells you the number of observations that match 
+# and mean(str_detect(x, pattern)) tells you the proportion that match
+
+babynames |> 
+  group_by(year) |> 
+  summarize(prop_x = mean(str_detect(name, "x"))) |> 
+  ggplot(aes(x = year, y = prop_x)) + 
+  geom_line()
+
+
+# 15.3.2 Count matches ---------------------------------------------------------
+
+x <- c("apple", "banana", "pear")
+str_count(x, "p")
+#> [1] 2 0 1
+
+
+str_count("abababa", "aba")
+#> [1] 2
+str_view("abababa", "aba")
+#> [1] │ <aba>b<aba>
+
+
+babynames |> 
+  count(name) |> 
+  mutate(
+    vowels = str_count(name, "[aeiou]"),
+    consonants = str_count(name, "[^aeiou]")
+  )
+
+
+
+# NOTE: regular expressions are case sensitive
+
+# One solution is to convert all letters to lower case...
+
+
+babynames |> 
+  count(name) |> 
+  mutate(
+    name = str_to_lower(name), # <<<<<<<<<<<<<<<<<<<<<
+    vowels = str_count(name, "[aeiou]"),
+    consonants = str_count(name, "[^aeiou]")
+  )
+
+
+# 15.3.3 Replace values --------------------------------------------------------
+# use with mutate()
+
+x <- c("apple", "pear", "banana")
+str_replace_all(x, "[aeiou]", "-")  # <<< replace
+
+x <- c("apple", "pear", "banana")
+str_remove_all(x, "[aeiou]")        # <<< remove
+
+
+# 15.3.4 Extract variables -----------------------------------------------------
+
+
+df <- tribble(
+  ~str,
+  "<Sheryl>-F_34",
+  "<Kisha>-F_45", 
+  "<Brandon>-N_33",
+  "<Sharon>-F_38", 
+  "<Penny>-F_58",
+  "<Justin>-M_41", 
+  "<Patricia>-F_84", 
+)
+
+
+df |> 
+  separate_wider_regex(
+    str,
+    patterns = c(
+      "<", 
+      name = "[A-Za-z]+", 
+      ">-", 
+      gender = ".",
+      "_",
+      age = "[0-9]+"
+    )
+  )
+
+
+# 15.4 Pattern details ---------------------------------------------------------
+
+# escaping, anchors, character classes, quantifiers, operator precedence and 
+# parentheses, and grouping components of the pattern
+
+
+# 15.4.1 Escaping -------------------------------------------------------------
+
+# To create the regular expression \., we need to use \\.
+dot <- "\\."
+dot
+
+# But the expression itself only contains one \
+str_view(dot)
+
+
+# And this tells R to look for an explicit .
+str_view(c("abc", "a.c", "bef"), "a\\.c")
+
+# more escape examples
+
+x <- "a\\b"
+str_view(x)
+
+my_string = str_view(x, "\\\\")
+print(my_string)
+
+# raw strings
+
+str_view(x, r"{\\}")
+
+# an alternative to using a backslash escape: you can use a character
+# class: [.], [$], [|], … all match the literal values.
+
+
+str_view(c("abc", "a.c", "a*c", "a c"), "a[.]c")
+
+
+
+str_view(c("abc", "a.c", "a*c", "a c"), ".[*]c")
+
+
+
+# 15.4.2 Anchors ---------------------------------------------------------------
+
+# ^ to match the start or $ to match the end:
+
+fruit
+
+str_view(fruit, "^a")
+
+str_view(fruit, "a$")
+
+
+
+str_view(fruit, "apple")
+
+str_view(fruit, "^apple$")
+
+
+# boundary between words with \b
+
+x <- c("summary(x)", "summarize(df)", "rowsum(x)", "sum(x)")
+
+x
+
+
+str_view(x, "sum")
+
+str_view(x, "\\bsum\\b")
+
+
+# When used alone, anchors will produce a zero-width match
+
+str_view("abc", c("$", "^", "\\b"))
+
+
+str_replace_all("abc", c("$", "^", "\\b"), "--")
+
+
+# 15.4.3 Character classes -----------------------------------------------------
+
+# A character class, or character set, allows you to match any character in a set.
+# Note: [^abc] matches any character *except* “a”, “b”, or “c”.
+# - defines a range, e.g., [a-z] matches any lower case letter and [0-9] matches any number.
+# \ escapes special characters, so [\^\-\]] matches ^, -, or ].
+# \d matches any digit;
+# \D matches anything that isn’t a digit.
+# \s matches any whitespace (e.g., space, tab, newline);
+# \S matches anything that isn’t whitespace.
+# \w matches any “word” character, i.e. letters and numbers;
+# \W matches any “non-word” character.
+
+x <- "abcd ABCD 12345 -!@#%."
+x
+
+str_view(x, "[abc]+")
+
+str_view(x, "[^a-z0-9]+")
+
+# You need an escape to match characters that are otherwise
+# special inside of []
+
+str_view("a-b-c", "[a-c]")
+
+str_view("a-b-c", "[a\\-c]")
+
+# see shortcuts above
+
+x <- "abcd ABCD 12345 -!@#%."
+x
+
+# \d matches any digit;
+str_view(x, "\\d+")
+
+# \D matches anything that isn’t a digit.
+str_view(x, "\\D+")
+
+# \s matches any whitespace (e.g., space, tab, newline);
+str_view(x, "\\s+")
+
+# \S matches anything that isn’t whitespace.
+str_view(x, "\\S+")
+
+# \w matches any “word” character, i.e. letters and numbers;
+str_view(x, "\\w+")
+
+# \W matches any “non-word” character.
+str_view(x, "\\W+")
+
+
+# 15.4.4 Quantifiers -----------------------------------------------------------
+
+# You can also specify the number of matches precisely with {}
+# {n} matches exactly n times.
+# {n,} matches at least n times.
+# {n,m} matches between n and m times.
+
+
+# 15.4.5 Operator precedence and parentheses ----------------------------------
+
+# quantifiers have high precedence and alternation has low precedence which 
+# means that ab+ is equivalent to a(b+), and ^a|b$ is equivalent to (^a)|(b$). 
+# Just like with algebra, you can use parentheses to override the usual order. 
+# But unlike algebra you’re unlikely to remember the precedence rules for 
+# regexes, so feel free to use parentheses liberally.
+
+
+# 15.4.6 Grouping and capturing ------------------------------------------------
+
+#  back reference: \1 refers to the match contained in the first parenthesis, 
+#  \2 in the second parenthesis, and so on
+
+# all fruit that has repeated pairs
+str_view(fruit, "(..)\\1")
+
+# all words that start and end with the same pair of letters
+str_view(words, "^(..).*\\1$")
+
+
+# using back references in str_replace()
+
+
+
+# this code switches the order of the second and third words in sentences
+
+str_view(sentences)
+
+sentences |> 
+  str_replace("(\\w+) (\\w+) (\\w+)", "\\1 \\3 \\2") |> 
+  str_view()
+
+# extracting the matches for each group using str_match(). 
+# Note - str_match() returns a matrix, so it’s not particularly easy 
+# to work with
+
+sentences |> 
+  str_match("the (\\w+) (\\w+)") |> 
+  head()
+
+# Converting to a tibble
+# separate_wider_regex() uses similar code behind the scenes
+
+sentences |> 
+  str_match("the (\\w+) (\\w+)") |> 
+  as_tibble(.name_repair = "minimal") |> 
+  set_names("match", "word1", "word2")
+
+
+# using parentheses without creating matching groups
+
+x <- c("a gray cat", "a grey dog")
+str_match(x, "gr(e|a)y")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
