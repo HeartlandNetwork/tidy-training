f130af875f572a31bad9a0767f1c3879659b83cf
HeartlandNetwork <gareth_rowell@nps.gov>
2024-06-11 09:21:38 -0500

    factors outline

---
+++ R4DS-2e/code_from_book/ch16-factors.R
@@ -0,0 +1,30 @@
+
+## Ch16 - Factors
+#===============================================================================
+
+# Factors - outline
+
+  # introduction----------------------------------------------------------------
+    # factor()
+    # gss_cat data 
+
+    # prerequisites
+      # base R
+      # forcats in tidyverse
+  
+  # factor basics --------------------------------------------------------------
+    # valid levels
+    # factor()
+    # forcats::fct()
+      # orders by first appearance:
+    # levels()
+    # readr::read_csv() 
+      # col_factor() option
+    
+  # General Social Survey ------------------------------------------------------
+    # NORC (Natl Opinion Research Center) University of Chicago
+    # gss_cat - small data subset
+
+    
+
+
