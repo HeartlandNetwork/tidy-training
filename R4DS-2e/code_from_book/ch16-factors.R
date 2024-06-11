c60ff299cf6fac954af5b48b12526568926d0530
HeartlandNetwork <gareth_rowell@nps.gov>
2024-06-11 10:10:49 -0500

    outline complete

--- R4DS-2e/code_from_book/ch16-factors.R
+++ R4DS-2e/code_from_book/ch16-factors.R
@@ -25,6 +25,26 @@
     # NORC (Natl Opinion Research Center) University of Chicago
     # gss_cat - small data subset
 
+  # Modifying factor order -----------------------------------------------------
+    # fct_reorder()
+    # fct_reorder2()
+    # fct_relevel()
+    # fct_infreq()
+    # fcr_rev()
+
+  # Modifying factor levels ----------------------------------------------------
+    # fct_recode()
+    # fct_collapse()  
+    # fct_lump_* family of functions
+    # fct_lump_n()
+
+  # Ordered factors ------------------------------------------------------------
+    # ordered()
     
+  # Summary --------------------------------------------------------------------
+    # Function reference in forcats
+    # Wrangling categorical data in R
+
+
 
 
