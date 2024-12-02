618f45764cd70680f5557b39b345854f7877632c
HeartlandNetwork <gareth_rowell@nps.gov>
2024-12-02 14:36:42 -0600

    Dates from components

--- R4DS-2e/code_from_book/Ch17-Dates_and_Times.R
+++ R4DS-2e/code_from_book/Ch17-Dates_and_Times.R
@@ -110,19 +110,117 @@ flights |>
 	
 
 
-  
-  
+make_datetime_100 <- function(year, month, day, time) {
+  make_datetime(year, month, day, time %/% 100, time %% 100)
+}
 
-  
 
+
+flights_dt <- flights |>
+  filter(!is.na(dep_time), !is.na(arr_time)) |>
+  mutate(
+    dep_time = make_datetime_100(year, month, day, dep_time),
+	arr_time = make_datetime_100(year, month, day, arr_time),
+	sched_dep_time = make_datetime_100(year, month, day, sched_dep_time),
+    sched_arr_time = make_datetime_100(year, month, day, sched_arr_time),
+  ) |>
+  select(origin, dest, ends_with("delay"), ends_with("time"))
+  
+  
+flights_dt
+
+# departure times across one year
+
+flights_dt |>
+  ggplot(aes(x = dep_time)) +
+  geom_freqpoly(binwidth = 86400) # 86400 seconds = 1 day
+  
+ # within a single day
+ 
+ 
+flights_dt |>
+  filter(dep_time < ymd(20130102)) |>
+  ggplot(aes(x = dep_time)) +
+  geom_freqpoly(binwidth = 600) # 600 seconds = 10 minutes
+ 
 
 # From Other Types
 
+# Switching between as_datetime() and as_date()
+
+as_datetime(today())
+
+as_date(now())
+
+# date/times as offsets from "Unix epoch" 1970-01-01
+
+as_datetime(60 * 60 * 10)
+
+as_date(365 * 10 + 2)
+
 
 # Date-Time Components
+flights_dt <- flights |>
+  filter(!is.na(dep_time), !is.na(arr_time)) |>
+  mutate(
+    dep_time = make_datetime_100(year, month, day, dep_time),
+	arr_time = make_datetime_100(year, month, day, arr_time),
+	sched_dep_time = make_datetime_100(year, month, day, sched_dep_time),
+    sched_arr_time = make_datetime_100(year, month, day, sched_arr_time),
+  ) |>
+  select(origin, dest, ends_with("delay"), ends_with("time"))
+  
+  
 # --------------------
 
 # Getting Components
+# accessor functions year(), month(), mday() day of the month
+# yday() day of the year, wday() (day of the week), hour(), 
+# minute(), second().
+
+
+
+datetime <- ymd_hms("2026-07-08 12:34:56")
+
+datetime
+
+year(datetime)
+
+month(datetime)
+
+mday(datetime)
+
+yday(datetime)
+
+wday(datetime)
+
+
+# label = TRUE gets abbreviated name
+
+month(datetime, label = TRUE)
+
+wday(datetime, label = TRUE, abbr = FALSE)
+
+#  see that there are more flights 
+#  during the week than in the weekend
+
+flights_dt
+
+flights_dt |>
+  mutate(wday = wday(dep_time, label = TRUE)) |>
+  ggplot(aes(x = wday)) +
+  geom_bar()
+  
+ 
+ 
+  
+  
+
+
+
+
+
+
 
 # Rounding
 
